8a30734321802536cefebcfd3bf1db4654f73323
Sakshi Agrawal <72269853+9406886320@users.noreply.github.com>
2026-07-25 10:50:43 +0530

    feat: add AppUser entity and repository

---
+++ employee-service/src/main/java/com/worksphere/employee/repository/AppUserRepository.java
@@ -0,0 +1,12 @@
+package com.worksphere.employee.repository;
+
+import com.worksphere.employee.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findByUsername(String username);
+
+}
